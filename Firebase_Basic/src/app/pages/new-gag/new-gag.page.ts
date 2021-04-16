b0237a2a75b68f1ab83322138bd908b2bf600e96
FedorMashoshin <fmashoshin@gmail.com>
2021-04-16 14:41:32 -0700

    Implement add image to gag functionality and store that img in the firebase

--- Firebase_Basic/src/app/pages/new-gag/new-gag.page.ts
+++ Firebase_Basic/src/app/pages/new-gag/new-gag.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { DataService, Gag } from 'src/app/services/data.service';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-new-gag',
@@ -15,6 +17,8 @@ export class NewGagPage implements OnInit {
     creator: null
   };
 
+  capturedImage = null;
+
   constructor(
     private dataService: DataService,
     private loadingCtrl: LoadingController,
@@ -24,11 +28,23 @@ export class NewGagPage implements OnInit {
   ngOnInit() {
   }
 
-  async save(){
+  async addImage(){
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      source: CameraSource.Photos,
+      resultType: CameraResultType.Base64
+    });
+    console.warn('Result IMG: ', image);
+    this.capturedImage = `data:image/jpeg;base64,${image.base64String}`;
+    this.gag.image = image.base64String;
+  }
+
+  async save(): Promise<void>{
     const loading = await this.loadingCtrl.create();
     await loading.present();
 
-    this.dataService.addGag(this.gag).then(res => {
+    this.dataService.addGag(this.gag).subscribe(res => {
       console.warn('Afted ADD: ', res);
       loading.dismiss();
       this.router.navigateByUrl('/');
--- Firebase_Basic/src/app/services/data.service.ts
+++ Firebase_Basic/src/app/services/data.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
 import * as firebase from 'firebase/app';
+import { from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 export interface Gag {
   title: string;
@@ -16,15 +19,25 @@ export interface Gag {
 export class DataService {
 
   constructor(
-    private angularFireStore: AngularFirestore
+    private angularFireStore: AngularFirestore,
+    private storage: AngularFireStorage
   ) { }
 
-  addGag(gag: Gag){
-    return this.angularFireStore.collection('gags').add({
-      title: gag.title,
-      creator: 'Fedor Mashoshin',
-      image: gag.image,
-      createdAt: firebase.default.firestore.FieldValue.serverTimestamp()
-    });
+addGag(gag: Gag){
+  const newName = `${new Date().getTime()}-SAMPLE.png`;
+  const storageRef: AngularFireStorageReference = this.storage.ref(`/gags/${newName}`);
+
+  // with from we transform promise into observable
+  const storageObs = from(storageRef.putString(gag.image, 'base64', { contentType: 'image/png' }));
+  return storageObs.pipe(
+    switchMap(obj => obj.ref.getDownloadURL()),
+    switchMap(url => this.angularFireStore.collection('gags').add({
+        title: gag.title,
+        creator: 'Fedor Mashoshin',
+        image: url,
+        createdAt: firebase.default.firestore.FieldValue.serverTimestamp()
+      }))
+  );
+
   }
 }
